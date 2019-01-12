308e88e16a305d146721f42086093b110c7815a1
Orlando <orlando.bel.jimmy@gmail.com>
2019-01-11 20:12:02 -0600

    haora registra el telefono en vezz de dejarlo vacio

--- SportsManagementSystem/app/Http/Controllers/SignupController.php
+++ SportsManagementSystem/app/Http/Controllers/SignupController.php
@@ -16,7 +16,7 @@ class SignupController extends Controller
 {
      public function __construct()
     {}
-    
+
     public function index()
     {
         $index=1;
@@ -25,95 +25,96 @@ class SignupController extends Controller
         ]);
     }
 
-   
-    
+
+
     public function getCompleto($B){
-        
+
         $index=4;
         $tlista = \App\carrera::where('id','<=',5)->lists('nombre','id');
         $tlistac = \App\carrera::where('id','>',6)->lists('nombre','id');
-        
+
         $user = \App\User::where('boleta','=',$B)->lists('boleta','id');
-             
-             
+
+
         return view('Registro.infoCompleta',[
         'index' => $index,
         'tlista' => $tlista,
         'tlistac' => $tlistac,
         'user' => $user
         ]);
-        
+
     }
-    
+
     public function postCompleto(Request $request){
         $this->validate($request, [
             'sexo' => 'required',
             'edad' => 'required',
-            
+
             'calle' => 'required',
             'ext' => 'required',
             'inter' => 'required',
             'colonia' => 'required',
             'cp' => 'required',
-            
+
             'insti' => 'required',
             'semestre'=> 'required',
             'grupo'=> 'required',
             'user'=> 'required',
             'tlista'=>'',
             'tlistac'=>'',
-        ]); 
-        
-        
+        ]);
+
+
         $Usuario = \App\User::find($request->user);
-        
+
         $Usuario->update([
             'completado' => 1,
         ]);
-        
+
         if($request->insti=="UPIIZ"){
-        
+
             informacion::create([
                 'usuario_id' => $request->user,
                 'institucion_id' => 1,
                 'carrera_id' => $request->tlista,
-                
-                'sexo' => $request->sexo,
+
+                'sexo' => $request->sexo -1,
                 'edad' => $request->edad,
-            
+
                 'calle' => $request->calle,
                 'numExterior' => $request->ext,
                 'numInterior' => $request->inter,
                 'colonia' => $request->colonia,
                 'codigoPostal' => $request->cp,
-            
+                'telefono' => $request->telefono,
+
                 'semestre' => $request->semestre,
                 'grupo' => $request->grupo,
-                
+
         ]);
         }else{
            informacion::create([
                 'usuario_id' => $request->user,
                 'institucion_id' => 2,
                 'carrera_id' => $request->tlistac,
-                
+
                 'sexo' => $request->sexo,
                 'edad' => $request->edad,
-            
+
                 'calle' => $request->calle,
                 'numExterior' => $request->ext,
                 'numInterior' => $request->inter,
                 'colonia' => $request->colonia,
                 'codigoPostal' => $request->cp,
-            
+
                 'semestre' => $request->semestre,
                 'grupo' => $request->grupo,
-                
-        ]); 
+
+        ]);
         }
-        
-        
+
+
         return redirect('/user');
-          
+
     }
 }
